1cc4eb65e691a1a2d2dcc0934e2f755c71478272
Ender Ruiz <ruiz.valera@gmail.com>
2023-01-13 13:27:53 -0300

    add gem sendgrid and method to send mail simple

--- app/controllers/estimate_steps_controller.rb
+++ app/controllers/estimate_steps_controller.rb
@@ -1,6 +1,10 @@
 class EstimateStepsController < ApplicationController
   # before_action :set_estimate
   include Wicked::Wizard
+
+  require 'sendgrid-ruby'
+  include SendGrid
+
   steps :step_one, :step_two
 
   def show
@@ -18,9 +22,27 @@ class EstimateStepsController < ApplicationController
   def update
     @estimate_form = current_estimate
     @estimate_form.update(estimate_params)
-
     redirect_to_finish_wizard
+    Rails.logger.info "antes del correo-----+++"
+    EstimateNotifierMailer.send_estimate_email(@estimate_form).deliver
+    # contact_client(@estimate_form)
+    Rails.logger.info "-------<Despues del correo"
   end
+
+
+# ----------------------esta es una prueba
+
+def contact_client(estimate)
+  from = SendGrid::Email.new(email: 'kimera.acces@gmail.com')
+  to = SendGrid::Email.new(email: 'kimera.acces@gmail.com')
+  subject = "Consulta de Cliente #{estimate.client_name}"
+  content = SendGrid::Content.new(type: 'text/plain', value: "#{estimate.client_phone}. Responder correo a #{estimate.client_email}")
+  mail = SendGrid::Mail.new(from, subject, to, content) 
+  sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+  response = sg.client.mail._('send').post(request_body: mail.to_json)
+end
+
+
 
   private 
 
